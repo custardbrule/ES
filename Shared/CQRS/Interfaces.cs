852c59cd675adf5a4daebcc560a7ad0901b19b54
Johnny.hoang <Johnny.hoang@terralogic.com>
2025-08-28 17:05:42 +0700

    init

---
+++ Shared/CQRS/Interfaces.cs
@@ -0,0 +1,27 @@
+﻿namespace CQRS;
+
+//void return 
+public struct Unit
+{
+    public static readonly Unit Value = new Unit();
+}
+public interface IRequest<Response> { }
+public interface IRequest : IRequest<Unit> { }
+
+public interface IHandler<Request, Response> where Request : IRequest<Response>
+{
+    Task<Response> Handle(Request request, CancellationToken cancellationToken);
+}
+public interface IHandler<Request> : IHandler<Request, Unit> where Request : IRequest<Unit> { }
+
+public interface IPipeline<TRequest, TResponse> where TRequest : IRequest<TResponse>
+{
+    Task Pre(TRequest request, CancellationToken cancellationToken = default);
+    Task Post(TRequest request, CancellationToken cancellationToken = default);
+}
+
+public interface IPublisher
+{
+    TResponse Send<TResponse>(IRequest<TResponse> request, CancellationToken cancellationToken = default);
+    TResponse Send<TRequest, TResponse>(TRequest request, CancellationToken cancellationToken = default) where TRequest : IRequest<TResponse>;
+}
---
+++ Shared/CQRS/Publisher.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CQRS
+{
+    public class Re:IRequest;
+    public class Publisher : IPublisher
+    {
+        private readonly IServiceProvider _serviceProvider;
+        public TResponse Send<TRequest, TResponse>(TRequest request, CancellationToken cancellationToken = default) where TRequest : IRequest<TResponse>
+        {
+            throw new NotImplementedException();
+        }
+
+        public TResponse Send<TResponse>(IRequest<TResponse> request, CancellationToken cancellationToken = default)
+        {
+            throw new NotImplementedException();
+        }
+
+        private IHandler<TRequest, TResponse> GetHanlder<TRequest, TResponse>(TRequest request) where TRequest : IRequest<TResponse>
+        {
+            var requestType = request.GetType();
+            var handlerType = typeof(IHandler<,>).MakeGenericType(requestType, typeof(TResponse));
+            var handler = _serviceProvider.GetService(handlerType);
+
+            if (handler is null)
+                throw new InvalidOperationException($"Handler not found for request type {requestType.Name}");
+
+            return handler as IHandler<TRequest, TResponse>;
+        }
+    }
+}
