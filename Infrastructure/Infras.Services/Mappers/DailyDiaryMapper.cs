c27461d01a141ca5f81f7b564929e48756a1c338
Johnny.hoang <Johnny.hoang@terralogic.com>
2025-09-12 16:53:43 +0700

    up

---
+++ Infrastructure/Infras.Services/Mappers/DailyDiaryMapper.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infras.Services.Mappers
+{
+    public static class DailyDiaryMapper
+    {
+        public static T Map<TSource, T>(this TSource source, Expression<Func<TSource, T>> mapperExpr) => mapperExpr.Compile()(source);
+    }
+}
