04789d33f9ac9347ddaeb31590843f1dd7c72473
Johnny.hoang <Johnny.hoang@terralogic.com>
2025-09-24 16:48:59 +0700

    update

--- Infrastructure/Infras.Services/Mappers/DailyDiaryMapper.cs
+++ Infrastructure/Infras.Services/Mappers/DailyDiaryMapper.cs
@@ -9,6 +9,24 @@ namespace Infras.Services.Mappers
 {
     public static class DailyDiaryMapper
     {
+        /// <summary>
+        /// use for mapping
+        /// </summary>
+        /// <typeparam name="TSource"></typeparam>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="source"></param>
+        /// <param name="mapperExpr"></param>
+        /// <returns></returns>
         public static T Map<TSource, T>(this TSource source, Expression<Func<TSource, T>> mapperExpr) => mapperExpr.Compile()(source);
+
+        /// <summary>
+        /// use for mapping
+        /// </summary>
+        /// <typeparam name="TSource"></typeparam>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="source"></param>
+        /// <param name="mapperFunc">function must be precompile if use for linq</param>
+        /// <returns></returns>
+        public static T Map<TSource, T>(this TSource source, Func<TSource, T> mapperFunc) => mapperFunc(source);
     }
 }
