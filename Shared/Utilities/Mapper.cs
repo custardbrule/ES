dc5afb30fd59df58b9f2505f462821a224cc5934
Johnny.hoang <Johnny.hoang@terralogic.com>
2026-02-02 16:53:12 +0700

    init ums-fe

--- Shared/Utilities/Mapper.cs
+++ Shared/Utilities/Mapper.cs
@@ -9,24 +9,7 @@ namespace Utilities
 {
     public static class Mapper
     {
-        /// <summary>
-        /// use for mapping in LINQ queries - uses direct expression invocation for SQL translation
-        /// </summary>
-        /// <typeparam name="TSource"></typeparam>
-        /// <typeparam name="T"></typeparam>
-        /// <param name="source"></param>
-        /// <param name="mapperExpr"></param>
-        /// <returns></returns>
-        public static T Map<TSource, T>(this TSource source, Expression<Func<TSource, T>> mapperExpr) => mapperExpr.Compile()(source);
-
-        /// <summary>
-        /// use for in-memory mapping - more efficient for non-queryable operations
-        /// </summary>
-        /// <typeparam name="TSource"></typeparam>
-        /// <typeparam name="T"></typeparam>
-        /// <param name="source"></param>
-        /// <param name="mapperFunc">function must be precompile if use for linq</param>
-        /// <returns></returns>
-        public static T MapMemory<TSource, T>(this TSource source, Func<TSource, T> mapperFunc) => mapperFunc(source);
+        public static IEnumerable<T> MapMemory<TSource, T>(this IEnumerable<TSource> sources, Func<TSource, T> mapperFunc) => sources.Select(mapperFunc);
+        public static IQueryable<T> Map<TSource, T>(this IQueryable<TSource> sources, Expression<Func<TSource, T>> mapperExpr) => sources.Select(mapperExpr);
     }
 }
